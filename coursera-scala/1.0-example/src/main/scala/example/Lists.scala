325e31c0f1fe67b88c24bc4f316f448e630c4f25
Mukesh Jha <mukh.007@gmail.com>
2017-01-28 23:56:28 -0700

    assingment 1

--- coursera-scala/1.0-example/src/main/scala/example/Lists.scala
+++ coursera-scala/1.0-example/src/main/scala/example/Lists.scala
@@ -23,7 +23,13 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+      if (xs.isEmpty) {
+        0
+      } else {
+        xs.head + sum(xs.tail)
+      }
+  }
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +44,17 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if (xs.isEmpty) throw new NoSuchElementException
+      else maxRecursive(xs.tail, xs.head)
+    }
+
+    def maxRecursive(xs: List[Int], largest: Int): Int = {
+      if (xs.nonEmpty) {
+        if (xs.head > largest) maxRecursive(xs.tail, xs.head)
+        else maxRecursive(xs.tail, largest)
+      } else {
+        largest
+      }
+    }
   }
